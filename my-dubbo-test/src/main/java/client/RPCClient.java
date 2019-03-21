e61f416474e80ffb68810ad68b974b7016166a34
menglili <mecuryhope@qq.com>
2019-03-21 15:57:27 +0800

    整合后台和统计脚本

--- my-dubbo-test/src/main/java/client/RPCClient.java
+++ my-dubbo-test/src/main/java/client/RPCClient.java
@@ -6,7 +6,6 @@ import service.HelloService;
 public class RPCClient {
     public static void main(String[] args) throws Exception{
         RpcProxy proxy = new RpcProxy();
-
         HelloService service =(HelloService) proxy.getProxyInstance(HelloService.class);
         System.out.println(service.test("hahahahhaha"));;
         proxy.destroy();
